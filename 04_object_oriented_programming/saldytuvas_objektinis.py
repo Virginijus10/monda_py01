d61d2aeee12f8b3b56bbebf2078008fb55968d79
virginijus10 <virginijus.baranausks@yahoo.com>
2024-01-09 14:36:24 +0200

    saldytuvasOOP

---
+++ 04_object_oriented_programming/saldytuvas_objektinis.py
@@ -0,0 +1,43 @@
+class Product:
+    def __init__(self, name:str, kiekis:float, **kwargs):
+            
+            self.unit_of_me
+            for key, value in kwargs:
+                setattr(self, key, value) 
+
+    def __str__(self) -> str:
+         return 
+
+
+    def __repr__(self) -> str:
+         return
+
+
+class Saldytuvas:
+    content = []
+
+    def check_product(self, name):
+        product = '' 
+
+    def check_product_quantity(self, ):   
+
+
+    def add_product(self, name, kiekis):
+        pass
+    
+
+    def remove_product(self, name:str, quantity:float):
+          pass
+
+    def print_contence(self):
+           pass 
+
+
+
+
+class Receptas:
+     ingredients = []
+     instructions = []  
+
+     def add_product(self, product):
+          
